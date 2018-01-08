66bb0b68579e7518c6665dcdd3f6f1ae68f3e220
Pagey <dan.page91@gmail.com>
2018-01-08 21:46:37 +0000

    Started designing and building modal.

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const modalBtn = document.getElementsByClassName('modal-btn');
+const modal = document.getElementsByClassName('modal-about');
+const button = document.getElementsByClassName('modal-cancel');
+
+modalBtn[0].addEventListener("click", function() {
+  modal[0].showModal();
+});
+
+button[0].addEventListener("click", function() {
+  modal[0].close();
+})
